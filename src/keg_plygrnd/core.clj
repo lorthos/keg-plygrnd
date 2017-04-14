a62100e1872d9ac793d5dc7b41be70c687803569
Halit Olali <halitolali@gmail.com>
2017-04-14 11:49:48 +0200

    sync

--- src/keg_plygrnd/core.clj
+++ src/keg_plygrnd/core.clj
@@ -1,10 +1,14 @@
 (ns keg-plygrnd.core
   (:require [powderkeg.core :as keg]
-            [net.cgrand.xforms :as x]))
+            [net.cgrand.xforms :as x])
+  :gen-class)
 
-(keg/connect! "spark://macbook-pro.home:7077")
-
-(def rdd (.textFile keg/*sc* "s3://lorthos-emr/*.csv"))
-
-(into []
-      (rdd (filter #(.contains % "spark"))))
+(defn -main []
+  (let [
+        ;conn (keg/connect! "yarn-cluster")
+        rdd (.textFile keg/*sc* "s3://lorthos-emr/*.csv")
+        courses (into []
+                      (rdd (filter #(.contains % "spark"))))]
+    (println courses)
+    )
+  )
